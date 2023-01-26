31ebdc6ed874444b92011526f9da1a110fe95228
imStav <saprocontact@gmail.com>
2023-01-26 11:21:02 +0100

    Implementing the user's profile section
    Created a ProfileView and a Profile component.
    Implemented the route path in src/router/index.js

---
+++ src/components/Profile.vue
@@ -0,0 +1,17 @@
+<script setup>
+import AppContainer from "./AppContainer.vue";
+</script>
+
+<template>
+  <AppContainer>
+    <div>
+        <h1>Test</h1>
+    </div>
+  </AppContainer>
+</template>
+
+<style scoped>
+h1 {
+    color: whitesmoke;
+}
+</style>
