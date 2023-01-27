78155106a17e6eddb822d546f83c20811d911c9b
imStav <saprocontact@gmail.com>
2023-01-27 13:23:31 +0100

    feature: implementing an `ImageGallery` component
    This component will list and render the user's posts in the Profile component.

---
+++ src/components/ImageGallery.vue
@@ -0,0 +1,31 @@
+<script setup>
+const props = defineProps({
+    posts: {
+        type: Array,
+    }
+});
+</script>
+
+<template>
+    <div class="gallery_container">
+        <img 
+            v-for="post in props.posts" 
+            :key="post.id" 
+            :src="post.image" 
+            alt="" 
+        />
+    </div>
+</template>
+
+<style scoped>
+img {
+    margin: 5px;
+    width: 200px;
+}
+
+.gallery_container {
+    display: flex;
+    justify-content: left;
+    flex-wrap: wrap;
+}
+</style>
--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AppContainer from "./AppContainer.vue";
+import ImageGallery from "./ImageGallery.vue";
 import UserBar from "./UserBar.vue";
 </script>
 
@@ -13,6 +14,23 @@ import UserBar from "./UserBar.vue";
           followers: 20,
           following: 20,
         }"
+      />
+
+      <ImageGallery 
+        :posts="[
+          {
+            id: 1,
+            image: 'https://images.unsplash.com/photo-1574169208507-84376144848b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=879&q=80'
+          },
+          {
+            id: 2,
+            image: 'https://images.unsplash.com/photo-1578309876267-d2205176bafd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1164&q=80'
+          },
+          {
+            id: 3,
+            image: 'https://images.unsplash.com/photo-1576767969134-4318b9bd84a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80'
+          },
+        ]"
       />
     </div>
   </AppContainer>
