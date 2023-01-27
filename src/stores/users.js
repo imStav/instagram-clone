a71fd8af2e0bfc95a8a48e0db47107ec47ab4697
imStav <saprocontact@gmail.com>
2023-01-27 15:05:32 +0100

    store: working on store
    Sarting to implement users related features such as login, sign up, user state, etc.

---
+++ src/stores/users.js
@@ -0,0 +1,20 @@
+import { ref } from "vue";
+import { defineStore } from "pinia";
+
+export const useUserStore = defineStore("user", () => {
+  const user = ref(null)
+  
+  const handleLogin = () => {
+  }
+
+  const handleSignup = () => {
+  }
+
+  const handleLogout = () => {
+  }
+
+  const getUser = () => {
+  }
+
+  return { user, handleLogin, handleSignup, handleLogout, getUser };
+});
