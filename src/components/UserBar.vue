8ed09ed42abb4414990bbe06f6c7e4141d082651
imStav <saprocontact@gmail.com>
2023-01-27 12:35:38 +0100

    UI: working on UserBar elements layout

--- src/components/UserBar.vue
+++ src/components/UserBar.vue
@@ -3,7 +3,15 @@
 
 <template>
     <div class="userbar_container">
-        <a-typography-title :level="2">Username</a-typography-title>
+        <div class="userbar_container_top">
+            <a-typography-title :level="2">Username</a-typography-title>
+        </div>
+
+        <div class="userbar_container_bottom">
+            <a-typography-title :level="5">0 posts</a-typography-title>
+            <a-typography-title :level="5">0 followers</a-typography-title>
+            <a-typography-title :level="5">0 following</a-typography-title>
+        </div>
     </div>
 </template>
 
@@ -15,5 +23,21 @@ h4,
 h5,
 p {
     color: whitesmoke;
+}
+
+.userbar_container {
+    display: flex;
+    flex-direction: column;
+}
+
+.userbar_container_bottom {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.userbar_container_bottom h5 {
+    margin-right: 1rem;
+    margin-top: 1rem;
 }
 </style>
