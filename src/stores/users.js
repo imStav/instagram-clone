a17154e9a6c826957e6a569937d3d892fcd3804d
imStav <saprocontact@gmail.com>
2023-01-27 16:38:03 +0100

    store: working on `handleSignup` (signup function)

--- src/stores/users.js
+++ src/stores/users.js
@@ -3,11 +3,36 @@ import { defineStore } from "pinia";
 
 export const useUserStore = defineStore("user", () => {
   const user = ref(null)
+  const errorMessage = ref("")
+
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
   
   const handleLogin = () => {
   }
 
-  const handleSignup = () => {
+  const handleSignup = (credentials) => {
+    const { email, password, username } = credentials;
+
+    const minUsernameLenght = 3;
+    const minPasswordLenght = 6;
+
+    if (username.lenght < minUsernameLenght) {
+      return errorMessage.value = `Username lenght must be at least ${minUsernameLenght} characters`
+    }
+
+    if (password.lenght < minPasswordLenght) {
+      return errorMessage.value = `Password lenght must be at least ${minPasswordLenght} characters`
+    }
+
+    if (!validateEmail(email)) {
+      return errorMessage.value = "Email is invalid"
+    }
   }
 
   const handleLogout = () => {
@@ -16,5 +41,5 @@ export const useUserStore = defineStore("user", () => {
   const getUser = () => {
   }
 
-  return { user, handleLogin, handleSignup, handleLogout, getUser };
+  return { user, errorMessage, handleLogin, handleSignup, handleLogout, getUser };
 });
